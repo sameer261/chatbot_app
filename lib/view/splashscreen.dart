d7d27670481752924107f38e4a2e8b8d0cb3cefc
Sameer Hussain <sameerhussain09473@gmail.com>
2025-04-29 21:21:27 +0500

    onboarding work start

--- lib/view/splashscreen.dart
+++ lib/view/splashscreen.dart
@@ -1,7 +1,6 @@
 import 'package:chatbot_app/controller/splashscreen_controller.dart';
 import 'package:chatbot_app/utils/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart'; // Make sure GetX is imported
 
 class Splashscreen extends StatelessWidget {
@@ -14,15 +13,7 @@ class Splashscreen extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: blue, // Set background color if needed
-      body: Center(
-        child: Center(
-          child: SvgPicture.asset(
-            'assets/images/ai.svg', // Replace with your image path
-            width: 100, // Image size
-            height: 100,
-          ),
-        ),
-      ),
+      body: Center(child: Image.asset('assets/images/robot.png')),
     );
   }
 }
